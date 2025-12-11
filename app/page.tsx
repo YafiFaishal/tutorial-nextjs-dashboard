02001bf897e5b28168e918a35e2ee0845d688dc2
Muhammad Yafi Faishal <l200240225@student.ums.ac.id>
2025-12-11 20:27:01 +0700

    Add dashboard pages and data fetching logic

--- app/page.tsx
+++ app/page.tsx
@@ -38,7 +38,6 @@ export default function Page() {
           />
         </div>
         <div
-          className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
         />
       </div>
     </main>
